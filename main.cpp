beb9cff96580fd936113e339db604d22a639cbd5
rfomin2006 <rfomin2805@gmail.com>
2024-09-15 15:25:51 +0700

    Fixed console messages

--- main.cpp
+++ main.cpp
@@ -5,11 +5,11 @@ int main() {
 	Vector v2(3.5, 7, 3);
 	Vector v3;
 	std::cout << "Vector1: " << v1 << std::endl;
-	std::cout << "Vector2" << v2 << std::endl;
+	std::cout << "Vector2: " << v2 << std::endl;
 	std::cout << "Sum of vectors: " << v1 + v2 << std::endl;
 	std::cout << "Vector1: " << v1 << std::endl;
-	std::cout << "Vector2" << v2 << std::endl;
+	std::cout << "Vector2: " << v2 << std::endl;
 	std::cin >> v3;
-	std::cout << "Vector3" << v3 << std::endl;
+	std::cout << "Vector3: " << v3 << std::endl;
 	return 0;
 }
